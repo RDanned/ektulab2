24555ecc025ed101f92b2d6dbbe3d2d8f8fe51dd
Ruslan <ruslanehg@gmail.com>
2021-04-04 21:50:21 +0600

    lab 6

---
+++ server.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+
+app.get('/skills/', function(req, res){
+    let skills = {data: [
+        {
+            name: "PHP",
+            value: "4",
+        },
+        {
+            name: "CSS",
+            value: "5",
+        },
+        {
+            name: "HTML",
+            value: "5",
+        },
+        {
+            name: "JS",
+            value: "4"
+        },
+        {
+            name: "React",
+            value: "2"
+        },
+        {
+            name: "Laravel",
+            value: "3"
+        }
+    ]};
+    res.json(skills);
+});
+
+app.get('/description/', function (req, res){
+    res.json({data: `
+        <h2 id="profile">Profile(from server)</h2>
+        <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        autem recusandae cumque cupiditate doloribus similique totam id
+        accusantium placeat, consequuntur praesentium fugit voluptatem
+        expedita, corporis laudantium ullam nulla dolore beatae cum? Iure
+        magni dolore ipsum veniam nobis sequi ullam optio porro atque
+        repudiandae nulla dignissimos minima, tempora accusamus maiores,
+        totam numquam?
+        </p>
+        <h2 id="experience">Experience</h2>
+        <span>One c rating</span><br />
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto
+        ipsum repellendus ullam sed obcaecati magnam rem modi veritatis
+        placeat earum?
+        </p>
+        <ul>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem, ipsum dolor.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae?</li>
+        </ul>
+    `});
+});
+
+app.get('/', function (req, res) {
+    var path = require('path');
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use(express.static(__dirname + '/'));
+
+var server = app.listen(8081, '127.0.0.1', function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at //%s:%s", host, port)
+})
